111c778fd4943f72f6d54c85c9c2cf6c8c764c0d
Eric Londaits <eric.londaits@gmail.com>
2019-02-10 10:56:28 -0300

    Add gulpfile and SASS support.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var public_dir = '.';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(public_dir + '/assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
