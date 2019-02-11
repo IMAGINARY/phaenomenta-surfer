85fa0b80b9f9a7fb0259c33ceec213c041946b74
Eric Londaits <eric.londaits@gmail.com>
2019-02-11 15:39:40 -0300

    Adds loader.

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,10 @@
-$('#redraw-button').on('click', function() {
-  $('.cindy-viewer iframe')[0].contentWindow.changeEquation($('#input-field').val());
+window.onSussLoaded = function() {
+  $('body').addClass('widget-loaded');
+}
+
+$(function() {
+  $('#redraw-button').on('click', function() {
+    $('.cindy-viewer iframe')[0].contentWindow.changeEquation($('#input-field').val());
+  });
 });
+
